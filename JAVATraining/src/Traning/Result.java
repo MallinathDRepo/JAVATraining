ac2c865d97cc4660229dcf3c38f0b9a94ecf1a96
mallinathGIT <mallinathdanali@gmail.com>
2023-04-17 14:46:46 +0530

    code push for Java Assignment 1

---
+++ JAVATraining/src/Traning/Result.java
@@ -0,0 +1,20 @@
+package Traning;
+
+public class Result {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=5;
+		
+		Maths m = new Maths();
+		m.add(a, b);
+		m.multiply(a, b);
+		m.sub(a, b);
+		m.div(a, b);
+		m.square(a, b);
+		m.cube(a, b);
+		
+		Scanner sc = new Scanner();
+		
+	}
+}
