6e95719172ec91c3dcca2db15cb5c77edc7089b5
mallinathGIT <mallinathdanali@gmail.com>
2023-04-20 15:30:18 +0530

    assignment 3 calculator with menu opetions - date: 19 april

---
+++ JAVATraining/src/calculator/Menu.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.io.IOException;
+
+public class Menu extends Maths{
+
+	public void mathMenu() {
+		System.out.println("--------- Select the operation to perform -----------");
+		System.out.println("1. Addition \n2. Substraction \n3. Multiplication \n4. Division \n5. Square \n6. Cube");
+		System.out.print("operation  = ");
+	}
+}
