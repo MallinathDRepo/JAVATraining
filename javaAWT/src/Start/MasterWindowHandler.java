b28c9327f81d80198d505885cfa36ab3292e22e5
mallinathGIT <mallinathdanali@gmail.com>
2023-05-05 13:32:45 +0530

    awt calculator code (partial)

---
+++ javaAWT/src/Start/MasterWindowHandler.java
@@ -0,0 +1,56 @@
+package Start;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class MasterWindowHandler implements WindowListener,MouseListener,MouseMotionListener {
+
+	@Override
+	public void mouseDragged(MouseEvent e) {}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		MyFrame mf = (MyFrame)e.getSource();
+		mf.dispose();		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+	
+}
--- javaAWT/src/Start/MyFrame.java
+++ javaAWT/src/Start/MyFrame.java
@@ -1,13 +1,73 @@
 package Start;
 
+import java.awt.Button;
 import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ComponentListener;
 
-public class MyFrame extends Frame{
-	// create own frame constructor to accept all required properties of FRAME
-	MyFrame(boolean visible, String title, int x, int y, int width, int height) {
-		this.setTitle(title);
-		this.setVisible(visible);
-		this.setBounds(x, y, width, height);
+import javax.swing.text.LabelView;
+
+public class MyFrame extends Frame {
+
 	
+	public MyFrame(boolean visible,String title, int x,int y, int width ,int height) {
+		this.setVisible(visible);
+		this.setTitle(title);
+		this.setBounds(x,y,width,height);
+		this.setLayout(null);
+		
+		Label firstNo, secondNo, result_text, result ;
+		
+		
+		/*  First no label and input text field */
+		firstNo = new Label("First No");
+		firstNo.setBounds(100,50,50,30);
+		add(firstNo);
+		
+		TextField firstTf = new TextField();
+		firstTf.setBounds(180,50,80,30);
+		add(firstTf);
+		
+		/* Second no label and input text field */
+		secondNo = new Label("Second No");
+		secondNo.setBounds(280,50,80,30);
+		add(secondNo);
+		TextField secondTf = new TextField();
+		secondTf.setBounds(380,50,80,30);
+		add(secondTf);
+		
+		/* Result label and text field */
+		result_text = new Label("Answer is : ");
+		result_text.setBounds(100,100,80,30);
+		add(result_text);
+		//result = result.getText();
+		
+		Button addButton = new Button("Add");
+		addButton.setBounds(200, 150, 80, 30);
+		
+		Button subButton = new Button("Subtract");
+		subButton.setBounds(350, 150, 80, 30);
+		
+		this.add(addButton);
+		this.add(subButton);
+		
+		addButton.addActionListener(new ActionHandler(firstTf,secondTf));
+		subButton.addActionListener(new ActionHandler(firstTf,secondTf));
+
+		// Single delegate for all types of events
+		this.addWindowListener(new MasterWindowHandler());
+		this.addMouseListener(new MasterWindowHandler());
+		this.addMouseMotionListener(new MasterWindowHandler());
+		
 	}
 }
+
+
+
+
+
+// multiple delgates for multiple events
+//this.addWindowListener(new WindowHandler());
+//this.addMouseListener(new MouseHandler());
+//this.addMouseMotionListener(new MouseMotionHandler());
