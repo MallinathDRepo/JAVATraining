cf4be0a566b0fba907b660a5eaced3f02d470ae8
mallinathGIT <mallinathdanali@gmail.com>
2023-05-26 18:46:59 +0530

    git update

--- javaAWT/src/com/employeeDetailsForm/controller/EmployeeHandler.java
+++ javaAWT/src/com/employeeDetailsForm/controller/EmployeeHandler.java
@@ -10,10 +10,11 @@ import com.employeeDetailsForm.Service.EmployeeService;
 
 public class EmployeeHandler implements ActionListener {
 
-	EmpFrameDesign ef ;
+	EmpFrameDesign ef;
 	EmployeeService employeeService;
+
 	public EmployeeHandler(EmpFrameDesign employeeFrame) {
-		ef= employeeFrame;
+		ef = employeeFrame;
 		employeeService = (EmployeeService) new EmployeeServiceImpl();
 	}
 
@@ -22,39 +23,36 @@ public class EmployeeHandler implements ActionListener {
 		// TODO Auto-generated method stub
 		String action = e.getActionCommand().toLowerCase();
 		EmployeeDTO dto = getEmployeeData(ef);
-		switch(action) {
-		case "save": 
-				employeeService.saveEmployee(dto);
-			        break;
+		switch (action) {
+		case "save":
+			employeeService.saveEmployee(dto);
+			break;
 		case "update":
-				System.out.println("update Clicked");
-				break;
+			System.out.println("update Clicked");
+			break;
 		case "search":
-				EmployeeDTO searchResult = employeeService.searchEmployee(dto);
-				System.out.println("This is queried from DB" + searchResult);
-				// refresh your UI
-			   break;
-			   
-		case  "delete":
+			EmployeeDTO searchResult = employeeService.searchEmployee(dto);
+			System.out.println("This is queried from DB" + searchResult);
+			// refresh your UI
+			break;
+
+		case "delete":
 			System.out.println("Delete Clicked");
-			  break;
-			        
+			break;
+
 		}
 	}
-	
-	
-	
-	public EmployeeDTO getEmployeeData(EmpFrameDesign ef2) {
 
+	public EmployeeDTO getEmployeeData(EmpFrameDesign ef2) {
 		EmployeeDTO empDTO = new EmployeeDTO();
 		empDTO.setEmployeeID(Integer.parseInt(ef2.getEmployeeIdTxt().getText()));
-		empDTO.setEmployeeName( ef2.getEmployeeNameTxt().getText());
+		empDTO.setEmployeeName(ef2.getEmployeeNameTxt().getText());
 		empDTO.setGender(ef2.getGenderGroup().getSelectedCheckbox().getLabel());
 		empDTO.setPf(ef2.getPfOption().getState());
 		empDTO.setGraduity(ef2.getGradutiyOption().getState());
 		empDTO.setMealCard(ef2.getMealCardOption().getState());
 		empDTO.setOfficeLocation(ef2.getOfficeLocation().getSelectedItem());
-		
+
 		System.out.println(empDTO);
 		return empDTO;
 	}
